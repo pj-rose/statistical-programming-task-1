494d90d4896ae18c676a9f7ec4632423e6ff685a
Jing Pan <pjedinburgh@163.com>
2022-10-02 09:40:48 +0100

    new q4

--- r code.R
+++ r code.R
@@ -10,9 +10,13 @@ a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers
 
 split_punct=function()
 {position=grep("\\W",a) #find the index that contains punctuations marks; return index
- b=rep(a[position]) 
+ x=rep("",length(a)+length(position))  #create a empty x, put strings in it
+ 
+ iis<-position+1:length(position) #indice to put the marks
+ x[iis]<-substr(a[position],-1,-2) 
  gsub(pattern="\\W"," ",a) #delete all the marks
   
+ 
 }
   
 
